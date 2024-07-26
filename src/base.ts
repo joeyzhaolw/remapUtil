d2e1508a2db097ef4f69e37608e6b43ad32885bb
zhaoliangwu <DmT0t5NcNU>
2024-07-26 09:22:54 +0800

    新增节流防抖函数

---
+++ src/base.ts
@@ -0,0 +1,51 @@
+/**
+ * @description 防抖函数
+ * @date 2024-07-25
+ * @param  fn 方法， delay  时间
+ * @returns 
+ */
+export const mydebounce = (fn: any, delay: number, immediate = false) => {
+	let timer = null
+	let isInvoke = false
+	const _debounce = function(...args: any[]) {
+		if (timer) clearTimeout(timer)
+		if (!isInvoke && immediate) {
+			fn.call(this, args)
+			isInvoke = true
+			return
+		}
+		timer = setTimeout(() => {
+			fn.call(this, args)
+			timer = null
+			isInvoke = false
+		}, delay)
+	}
+	_debounce.cancel = function() {
+		if (timer) clearTimeout(timer)
+		timer = null
+		isInvoke = false
+	}
+	return _debounce
+}
+
+/**
+ * @description 节流函数 myThrottle(fn, 500)
+ * @date 2024-07-25
+ * @param  fn 方法， delay  时间
+ * @returns 
+ */
+export const myThrottle = (fn: any, interval: number, immediate = true) => {
+	let startTime = 0
+	const _throttle = function(...args: any[]) {
+		const nowTime = new Date().getTime()
+		if (!immediate && startTime === 0) {
+			startTime = nowTime
+		}
+		const waitTime = interval - (nowTime - startTime)
+		if (waitTime <= 0) {
+			fn.apply(this, args)
+			startTime = nowTime
+		}
+	}
+	return _throttle
+}
